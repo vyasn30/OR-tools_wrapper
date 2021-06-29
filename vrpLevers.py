e28dc917194922a956131e4b472229f6fe64796f
Naman Kaushikbhai Vyas <arch4ng3l@pop-os.localdomain>
2021-06-29 12:28:43 +0530

    simple vrp solved, with cartesian coors and eucl distance

--- vrpLevers.py
+++ vrpLevers.py
@@ -15,22 +15,102 @@ class Coors:
     self.x = x
     self.y = y
 
+class TimingWindow:
+  def __init__(self, startTime, endTime):
+    self.startTime =  startTime
+    self.endTime = endTime
+    
+  
 
 class Node:
-  def __init__(self, coors):
+  def __init__(self, coors, timingWindow=None):
+    self.timingWindow = timingWindow
     self.coors = coors
+    self.id = id(self)
+
+
+class Network:
+  def __init__(self, nodes, depotNode, numVehicles):
+    self.nodes = nodes
+    self.depot = depotNode
+    self.numVehicles = numVehicles
+    print(self.numVehicles)
   
+
 class DataModel:
-  def __init__(self, nodes):
+  def __init__(self, network):
     self.data = {}
-    self.nodes = nodes 
+    self.network = network
+    self.data["num_vehicles"] = self.network.numVehicles
+    self.data["depot"] = self.network.depot
+    
 
 
   def calculateDistanceMatrix(self):
-    nodeCoorList =[[node.coors.x, node.coors.y] for node in nodes]
-    return distance_matrix(nodeCoorList, nodeCoorList)
+    nodeCoorList =[[node.coors.x, node.coors.y] for node in network.nodes]
 
+    self.data["distance_matrix"] = distance_matrix(nodeCoorList, nodeCoorList)
+      
   
+
+
+  def getData(self):
+    self.calculateDistanceMatrix()
+    return self.data
+  
+  
+
+
+class vrpWrap:
+  def __init__(self, data):
+    self.data = data
+    self.manager = pywrapcp.RoutingIndexManager(len(data["distance_matrix"]), data["num_vehicles"], data["depot"])
+    self.routingManager = pywrapcp.RoutingModel(self.manager)
+    self.transit_callback_index = None 
+
+
+  def addDistanceDimension(self):
+    self.routingManager.AddDimension(
+        self.transit_callback_index,
+        0,  # no slack
+        3000,  # vehicle maximum travel distance
+        True,  # start cumul to zero
+        "Distance"
+      )
+    
+    distance_dimension = self.routingManager.GetDimensionOrDie("Distance")
+    distance_dimension.SetGlobalSpanCostCoefficient(100)
+
+
+
+  def distanceCallback(fromIndex, toIndex):
+    fromNode = self.manager.IndextoNode(fromIndex)
+    toNode = self.manager.IndextoNode(toIndex)
+
+    return self.data['distance_matrix'][fromNode][toNode]
+
+  def solve(self):
+    self.transit_callback_index = self.routingManager.RegisterTransitCallback(self.distanceCallback)
+
+    self.routingManager.SetArcCostEvaluatorOfAllVehicles(self.transit_callback_index)
+    
+    self.addDistanceDimension()
+    
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.first_solution_strategy = (
+        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+
+    solution = self.routingManager.SolveWithParameters(search_parameters)
+
+    return solution
+
+
+
+
+    
+
+
+
 
 if __name__ == '__main__':
   nodes = []
@@ -47,8 +127,12 @@ if __name__ == '__main__':
   nodes.append(node2)
   nodes.append(node3)
 
-  print(type(nodes))
+  for val in nodes:
+    print(val.id)
+
+ 
+  network =  Network(nodes, 0, 1)
 
 
-  print(DataModel(nodes).calculateDistanceMatrix())
-
+  vrp = vrpWrap(DataModel(network).getData())
+  print(vrp.solve())
