232253280a170f6971e1e94efbfa8abc82b8583b
vyasn30 <arch4ng3l@pop-os.localdomain>
2021-06-29 14:54:47 +0530

    Coors now in at long

--- vrpLevers.py
+++ vrpLevers.py
@@ -5,15 +5,15 @@ import math
 import numpy as np
 import pandas as pd
 from scipy.spatial import distance_matrix
-
+from sklearn.neighbors import DistanceMetric
 
 def calculateDistance(coor1, coor2):
   return math.sqrt((coor1.x-coor2.x)**2 + (coor1.y-coor2.y)**2)
 
 class Coors:
-  def __init__(self, x, y):
-    self.x = x
-    self.y = y
+  def __init__(self, latitude, longitude):
+    self.latitude = latitude
+    self.longitude = longitude
 
 class TimingWindow:
   def __init__(self, startTime, endTime):
@@ -47,11 +47,14 @@ class DataModel:
 
 
   def calculateDistanceMatrix(self):
-    nodeCoorList =[[node.coors.x, node.coors.y] for node in network.nodes]
+    nodeCoorList =[[np.radians(node.coors.latitude), np.radians(node.coors.longitude)] for node in network.nodes]
+    
 
-    self.data["distance_matrix"] = distance_matrix(nodeCoorList, nodeCoorList)
+    metric = DistanceMetric.get_metric("haversine")
+    self.data["distance_matrix"] =  metric.pairwise(nodeCoorList)*6373
+
       
-  
+     
 
 
   def getData(self):
@@ -115,9 +118,10 @@ class vrpWrap:
 if __name__ == '__main__':
   nodes = []
 
-  coor1 = Coors(5,7)
-  coor2 = Coors(7,3)
-  coor3 = Coors(8,2)
+  coor1 = Coors(12.9716,77.5946)
+  coor2 = Coors(19.076,72.877)
+  coor3 = Coors(28.7041,77.1025)
+  coor4 = Coors(13.0827, 80.2707)
 
   node1 = Node(coor1)
   node2 = Node(coor2)
@@ -132,6 +136,8 @@ if __name__ == '__main__':
 
  
   network =  Network(nodes, 0, 1)
+  print(DataModel(network).getData())
+
 
 
   vrp = vrpWrap(DataModel(network).getData())
