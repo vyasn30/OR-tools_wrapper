5b25e8d33c98b3e2e6b4cf21aaa9467585d3a4da
vyasn30 <arch4ng3l@pop-os.localdomain>
2021-06-30 18:12:00 +0530

    Demand for all vehicles is same, giving it as a list causes error

--- vrpLevers.py
+++ vrpLevers.py
@@ -1,8 +1,5 @@
 #Now we shall be working on capacity constraints
 
-
-
-
 from ortools.constraint_solver import routing_enums_pb2
 from ortools.constraint_solver import pywrapcp
 import time
@@ -12,12 +9,6 @@ import pandas as pd
 from scipy.spatial import distance_matrix
 from sklearn.neighbors import DistanceMetric
 from geopy.geocoders import Nominatim
-
-
-def calculateDistance(coor1, coor2):
-  return math.sqrt((coor1.x-coor2.x)**2 + (coor1.y-coor2.y)**2)
-
-
 
 class Coors:
   def __init__(self, latitude=None, longitude=None, geoString = None):
@@ -32,6 +23,9 @@ class Coors:
       print(location.latitude)
       print(location.longitude)
    
+class Vehicle:
+  def __init__(self, capacity):
+    self.capacity = capacity
 
    
 class TimingWindow:
@@ -42,24 +36,28 @@ class TimingWindow:
   
 
 class Node:
-  def __init__(self, coors, timingWindow=None):
-    self.timingWindow = timingWindow
+  def __init__(self, coors, demand = None):
     self.coors = coors
     self.id = id(self)
-
+    self.demand = demand
 
 class Network:
-  def __init__(self, depotNode, numVehicles, nodes = None):
+  def __init__(self, depotNode, numVehicles, nodes = None, vehicles = None):
     self.nodes = []
     self.depot = depotNode
     self.numVehicles = numVehicles
+    self.vehicles = vehicles
     print(self.numVehicles)
 
-  def addNode(self, coors):
+  
+  def addNodeToNetwork(self, node):
+    self.nodes.append(node)
+
+
+  def addNodeFromCoors(self, coors):
     newNode = Node(coors)
     self.nodes.append(newNode)
-    
-  
+
 
 class DataModel:
   def __init__(self, network):
@@ -68,23 +66,34 @@ class DataModel:
     self.data["num_vehicles"] = self.network.numVehicles
     self.data["depot"] = self.network.depot
     self.data["names"] = [] 
- 
+    self.data["demands"] = []
+    self.data["vehicle_capacities"] = []
+
+  
 
 
   def calculateDistanceMatrix(self):
-    print([[node.coors.latitude,node.coors.longitude] for node in self.network.nodes]) 
     nodeCoorList =[[np.radians(node.coors.latitude), np.radians(node.coors.longitude)] for node in self.network.nodes]
     
-    print(nodeCoorList)
     metric = DistanceMetric.get_metric("haversine")
     self.data["distance_matrix"] =  metric.pairwise(nodeCoorList)*6373
 
   def assignNames(self):
     self.data["names"] = [node.coors.geoString for node in self.network.nodes]
      
+  def setDemands(self):
+    for node in self.network.nodes:
+      self.data["demands"].append(node.demand)
+
+  def setVehicleCapacities(self):
+    for vehicle in self.network.vehicles:
+      self.data["vehicle_capacities"].append(vehicle.capacity)
 
 
   def getData(self):
+    print("Here")
+    self.setVehicleCapacities()
+    self.setDemands()
     self.assignNames()
     self.calculateDistanceMatrix()
     return self.data
@@ -99,6 +108,8 @@ class vrpWrap:
     self.routingManager = pywrapcp.RoutingModel(self.manager)
     self.transit_callback_index = None 
     self.solution = None
+    self.demand_callback_index = None
+
 
   def addDistanceDimension(self):
     self.routingManager.AddDimension(
@@ -113,22 +124,54 @@ class vrpWrap:
     distance_dimension.SetGlobalSpanCostCoefficient(100)
 
 
+  def addCapacityDimension(self):
+    self.routingManager.AddDimension(
+      self.demand_callback_index,
+      0,  
+      15, 
+      True,
+      "Capacity"
+    )
+
 
   def distanceCallback(self, fromIndex, toIndex):
     fromNode = self.manager.IndexToNode(fromIndex)
     toNode = self.manager.IndexToNode(toIndex)
     return self.data['distance_matrix'][fromNode][toNode]
 
+
+  def demandCallback(self, fromIndex):
+    fromNode = self.manager.IndexToNode(fromIndex)
+    return self.data['demand'][fromNode]
+  
+
+
   def solve(self):
     self.transit_callback_index = self.routingManager.RegisterTransitCallback(self.distanceCallback)
 
     self.routingManager.SetArcCostEvaluatorOfAllVehicles(self.transit_callback_index)
+   
     
+    self.demand_callback_index = self.routingManager.RegisterTransitCallback(
+      self.demandCallback
+    )
+
+
     self.addDistanceDimension()
-    
+    self.addCapacityDimension()
+
+
+
     search_parameters = pywrapcp.DefaultRoutingSearchParameters()
     search_parameters.first_solution_strategy = (
-        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
+    )
+    
+    search_parameters.local_search_metaheuristic = (
+        routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH
+    )
+
+    search_parameters.time_limit.FromSeconds(1)
 
     self.solution = self.routingManager.SolveWithParameters(search_parameters)
 
@@ -164,23 +207,32 @@ class vrpWrap:
 
 if __name__ == '__main__':
   nodes = []
-  coors = [Coors(geoString = "Ambawadi Circle, Ahmedabad"),Coors(geoString = "Club 07, Bopal")
-           ,Coors(geoString = "Naroda Patiya"),Coors(geoString = "The Fern Hotel, Sola")
-           ,Coors(geoString = "Trimandir, Adalaj")]
+  vehicleNumber = 1
+  depotNode = 0
+  
+  
+  vehicleS = [Vehicle(15)]
 
- 
-  network =  Network(0,1)
+  coors = [Coors(geoString = "Ambawadi Circle, Ahmedabad"),
+           Coors(geoString = "Club 07, Bopal"),
+           Coors(geoString = "Naroda Patiya"),
+           Coors(geoString = "The Fern Hotel, Sola"),
+           Coors(geoString = "Trimandir, Adalaj")]
 
-  for coor in coors:
-    network.addNode(coor)    
+  demands = [0, 3, 5, 2, 6] 
 
+  network =  Network(depotNode,vehicleNumber,vehicles=vehicleS)
 
+  for i in range(0, len(coors)):
+    newNode = Node(coors[i], demands[i])
+    network.addNodeToNetwork(newNode)
+
+  
   print(DataModel(network).getData())
-
-
 
   vrp = vrpWrap(DataModel(network).getData())
   solution = vrp.solve()
   print(solution)
   print(vrp.routingManager.GetArcCostForVehicle(0, 1, 0))
   print(vrp.print_solution())
+
